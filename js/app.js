031615c82c2fc99ee2044a877dfa83a121593807
Brittany Wirtz <brittanyjhanson@gmail.com>
2017-02-04 14:27:18 -0800

    Attach titles to counter

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
-var ViewModel = function() {
+var Cat = function() {
   var self = this;
   self.clickCount = ko.observable(0);
   self.name = ko.observable('Cutie');
   self.imgSrc = ko.observable('img/dog1.jpeg');
-  // self.stages = ko.observableArray([
-  //   {threshold: 0, stage: 'Newborn'},
-  //   {threshold: 5, stage: 'Child'},
-  //   {threshold: 13, stage: 'Teenager'},
-  //   {threshold: 21, stage: 'Adult'},
-  //   {threshold: 50, stage: 'Senior'}
-  // ]);
-
-  self.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
-    self.nickName = ko.observableArray([
+  self.nickName = ko.observableArray([
       { name: 'Doggy'},
       { name: 'Pumpkin'},
       { name: 'Poops'}
-    ])
+    ]);
+  self.title = ko.computed(function() {
+    var title;
+    var clicks = self.clickCount();
+    if (clicks < 5) {
+      title = "Newborn";
+    } else if (clicks < 13) {
+      title = "Child";
+    } else if (clicks < 21) {
+      title = "Teenager";
+    } else if (clicks < 50) {
+      title = "Adult";
+    } else {
+      title = "Senior"
+    }
+    return title;
+  })
+}
 
+var ViewModel = function() {
+  self.currentCat = ko.observable(new Cat());
+  self.incrementCounter = function() {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
 }
 
 ko.applyBindings(new ViewModel());
